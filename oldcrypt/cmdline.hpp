24d812053814bd65ab5138cc78e62b520a8fc38b
checkmatemusipan <checkmatemusipan@fireashotrailgun81270.yahoo.co.jp>
2021-02-21 11:50:22 +0900

    add socket

---
+++ oldcrypt/cmdline.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+#ifndef __my_cmdline
+#define __my_cmdline
+
+class my_cmdline {
+  private:
+    
+};
+
+
+#endif
+
---
+++ oldcrypt/interactive.hpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+#ifndef __interactive_shell
+#define __interactive_shell
+
+class my_shell{
+  private:
+    hoge
+  public:
+};
+
+#endif
+//menu exec_proc socket crypt disas
+/*
+ * interactive shell
+ * menu
+ * > "num or string"
+ *
+ * #socket
+ * host >
+ * port >
+ * message >p64()+p32()+string
+ * recv >
+ * message >
+ * recv >
+ * 
+ * #crypt(rot13,viju2d,..)
+ * crypt(??) > encode(plain)
+ * crypt(??) > decode(enc,key)
+ * crypt(??) > try_decode(enc,key,if_in_word) key can be empty
+ *
+ * #exec_proc
+ * cmd(raw) > ls -al > filename
+ * cmd(raw) > ls -al
+ * cmd(result) > string..
+ *
+ * #disas
+ *
+ */
+
